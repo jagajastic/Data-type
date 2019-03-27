47ddc6aba5f4a4b3163f23e68faac35796d9da90
ibrahim <josephibrahi@gmail.com>
2019-03-27 13:59:58 +0100

    Boolean(the true or false):  added a boolen value of either true or false

---
+++ boolean.js
@@ -0,0 +1,17 @@
+/**
+ * Author : Ibrahim Joseph M.
+ * Data type: Boolean 
+ * 
+ * 
+ */
+
+// testing true and false value
+
+var raining = true;
+
+if (raining) {
+    console.log("Its raining!");
+} else {  // second case is false 
+    console.log("No rain");
+}
+
